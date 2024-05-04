4bb40ed9cb0946fed3a2d705156cc82f011ba0ac
Samansh <samansh1999@gmail.com>
2024-05-05 01:43:04 +0530

    feat: button loader

--- App.tsx
+++ App.tsx
@@ -1,117 +1,141 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useMemo} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  LayoutRectangle,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import Animated, {
+  cancelAnimation,
+  Easing,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App(): React.JSX.Element {
+  const [dimensions, setDimensions] = React.useState<LayoutRectangle>({
+    height: 0,
+    width: 0,
+    x: 0,
+    y: 0,
+  });
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const [showLoader, setShowLoader] = React.useState(false);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const scale = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    const diagonal = (dimensions.width ** 2 + dimensions.height ** 2) ** 0.5;
+
+    return {
+      height: diagonal,
+      width: diagonal,
+      opacity: interpolate(scale.value, [0, 0.7, 0.9], [1, 1, 0]),
+      transform: [{scale: scale.value}],
+      borderRadius: diagonal / 2,
+    };
+  });
+
+  const childAnimatedStyles = useAnimatedStyle(() => ({
+    transform: [{scale: scale.value > 0 ? 1 / scale.value : 0}],
+  }));
+
+  const startAnimation = () => {
+    setShowLoader(true);
+    scale.value = withRepeat(
+      withSequence(
+        withTiming(1, {duration: 1000, easing: Easing.linear}),
+        withTiming(0, {duration: 1000, easing: Easing.cubic}),
+      ),
+      -1,
+    );
+    setTimeout(() => {
+      cancelAnimation(scale);
+      scale.value = 0;
+      setShowLoader(false);
+    }, 10000);
+  };
+
+  const onPress = () => {
+    startAnimation();
+  };
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <View style={{height: 200}} />
+      <TouchableOpacity
+        onLayout={e => {
+          setDimensions(e.nativeEvent.layout);
+        }}
+        onPress={onPress}
+        style={styles.btn}>
+        <Text style={styles.btnText}>Click me</Text>
+      </TouchableOpacity>
+      {showLoader && (
+        <View
+          style={[
+            styles.rippleWrapper,
+            {
+              height: dimensions.height,
+              width: dimensions.width,
+              top: dimensions.y,
+              left: dimensions.x,
+            },
+          ]}>
+          <Animated.View style={[styles.rippleView, animatedStyles]}>
+            <Animated.View style={[styles.btn2, childAnimatedStyles]}>
+              <Text style={styles.btn2Text}>Click me</Text>
+            </Animated.View>
+          </Animated.View>
+        </View>
+      )}
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  btn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: 'cyan',
+    borderRadius: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  btnText: {
+    color: 'blue',
   },
-  highlight: {
-    fontWeight: '700',
+  btn2: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: 'black',
+  },
+  btn2Text: {
+    color: 'white',
+  },
+  rippleView: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+    overflow: 'hidden',
+    position: 'absolute',
+  },
+  rippleWrapper: {
+    position: 'absolute',
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
   },
 });
 
